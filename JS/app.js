21afb468075ab983d5c1aebec786081a47539c17
Steven Weeks <Steven_weeks2001@yahoo.com>
2018-09-18 11:11:15 -0700

    finChanges

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,4 @@
-// Grab the gallery to append to it
-let gallery = document.getElementById('gallery')
-// create a variable to later put each profile into
-let cardContent = ''
-
 // fetching the data from randomuser api
-
 fetch('https://randomuser.me/api?results=12&nat=US')
 // the response is then turned into json so it can be easier to work with
   .then(response => response.json())
@@ -13,9 +7,9 @@ fetch('https://randomuser.me/api?results=12&nat=US')
     profileMaker(data.results)
     // this adds a click listener to the document
     document.addEventListener('click', (e) => {
-      // this then limits the clicks to children of the card div closest() targets the closest parent/ancestor
+      // closest('.card') restricts the click eventlistener to the children of the card div
       if (e.target.closest('.card')) {
-        //
+        // index variable grabs the id (given in the profileMaker()) of the card div
         let index = (e.target.closest('.card').id)
         let person = data.results
         // pass in json info from api, and the id of the card clicked.
@@ -26,6 +20,10 @@ fetch('https://randomuser.me/api?results=12&nat=US')
 
 // Create the profiles that will be displayed when page loads.
 function profileMaker (infos) {
+  // Grab the gallery to change it's innerHTML
+  let gallery = document.getElementById('gallery')
+  // create a variable to later put each profile into
+  let cardContent = ''
   // goes through each person object and displays the requested info on the cards, and gives each card an id number
   infos.forEach((person, index) => {
     cardContent += `<div class="card" id="${index}">
@@ -42,23 +40,24 @@ function profileMaker (infos) {
   // once done filling each card, it then gets put on the gallery to display
   gallery.innerHTML = cardContent
 }
+
 // function that makes the modals; more detailed version of the cards.
 // passes index and an array people objects to make going through them easier.
-function modalMaker( index, peopleArr) {
+function modalMaker (index, peopleArr) {
   // just some css fluff to make the background behind the current modal appear blurry
-document.querySelector('.gallery').classList.add('blur')
-document.querySelector('header').classList.add('blur')
-// person variable to make it easier to use template literals
-let person = peopleArr[index]
-// these two variables set up the birthdate to match with the mockups
-let noTime = `${person.dob.date}`
-let reverseDate = `${noTime.charAt(5)}${noTime.charAt(6)}/${noTime.charAt(8)}${noTime.charAt(9)}/${noTime.charAt(2)}${noTime.charAt(3)}`
-// these three variables make the phone number appear as in the mockups
-let cell = `${person.cell}`.split("").splice(0,5).join("")
-let cellTwo = `${person.cell}`.split("").splice(6,10).join("")
-let cellThree = `${cell} ${cellTwo}`
-// variable that serves as a template for what a modal should have inside of it, using template literals to use info gained from the api
-let modal =
+  document.querySelector('.gallery').classList.add('blur')
+  document.querySelector('header').classList.add('blur')
+  // person variable to make it easier to use template literals
+  let person = peopleArr[index]
+  // these two variables set up the birthdate to match with the mockups
+  let noTime = `${person.dob.date}`
+  let reverseDate = `${noTime.charAt(5)}${noTime.charAt(6)}/${noTime.charAt(8)}${noTime.charAt(9)}/${noTime.charAt(2)}${noTime.charAt(3)}`
+  // these three variables make the phone number appear as in the mockups
+  let cell = `${person.cell}`.split("").splice(0,5).join("")
+  let cellTwo = `${person.cell}`.split("").splice(6,10).join("")
+  let cellThree = `${cell} ${cellTwo}`
+  // variable that serves as a template for what a modal should have inside of it, using template literals to use info gained from the api
+  let modal =
 `<div class="modal-container">
       <div class="modal">
           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -78,18 +77,18 @@ let modal =
           <button type="button" id="modal-next" class="modal-next btn">Next</button>
       </div>
   </div>`
+  
   // gotta make a new div, then append it to the body then make it's innerHTML become the modal template.
   let mody = document.createElement('div')
   document.body.appendChild(mody)
   mody.innerHTML = modal
-// click listeners for the next, prev and close btns.  Stops user from continuing if index is at either end of the index.
+  // click listeners for the next, prev and close btns.  Stops user from continuing if index is at either end of the index.
   document.getElementById('modal-next').addEventListener('click', (e) => {
     // had to use == instead of ===, === would allow attempts to go back or forward if you started at 0 or 11.
     if (index == 11) { return }
     let indexAdd = parseInt(index) + 1;
     e.target.closest('.modal-container').remove()
     modalMaker(indexAdd, peopleArr)
-
   })
   document.getElementById('modal-prev').addEventListener('click', (e) => {
     if (index == 0) { return }
@@ -124,15 +123,15 @@ document.getElementById('search-submit').addEventListener('click', (e) => {
 // uses RegExp to search the values provided, and does so globally so it doesn't just return the first one found.
 // forEach goes through each name on the cards profiles, and compares it to the RegExp of the values provides from the search input
 // leaves the display as is for matching, and none for non-matching.
-  function searcher (letts) {
-    let regEx = new RegExp(letts, 'gi')
-    let named = Array.from(document.querySelectorAll('.card-name'))
-    named.forEach(namer => {
-      let compareName = namer.innerHTML
-      if (compareName.match(regEx)) {
-        namer.closest('.card').style.display = ""
-      } else {
-        namer.closest('.card').style.display = "none"
-      }
-    })
-  }
+function searcher (letts) {
+  let regEx = new RegExp(letts, 'gi')
+  let named = Array.from(document.querySelectorAll('.card-name'))
+  named.forEach(namer => {
+    let compareName = namer.innerHTML
+    if (compareName.match(regEx)) {
+      namer.closest('.card').style.display = ""
+    } else {
+      namer.closest('.card').style.display = "none"
+    }
+  })
+}
